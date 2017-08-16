8c200b25a9e88414f8f4b9f55071da2288e3e53e
Pranav Nutalapati <pranavnutalapati@gmail.com>
2017-08-16 16:34:09 +0530

    Reorganized files.

--- src/prob00.java
+++ src/prob00.java
@@ -1,5 +1,3 @@
-package com.preyneyv.codewars2k16;
-
 public class prob00 {
     public static void main(String[] args) {
         System.out.println("Arkansas, The Natural State!");
--- src/prob06.java
+++ src/prob06.java
@@ -1,5 +1,3 @@
-package com.preyneyv.codewars2k16;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
